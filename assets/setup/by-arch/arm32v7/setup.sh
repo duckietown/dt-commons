7fa5f840ddecaec496988417921221e20020cb60
Andrea F. Daniele <afdaniele@ttic.edu>
2022-07-06 11:44:59 -0500

    dropped arm32v7 architecture

--- assets/setup/by-arch/arm32v7/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# make the VC library reachable
-cd /opt/ || exit
-tar zxvf ./vc.tgz
-echo /opt/vc/lib > /etc/ld.so.conf.d/00-vmcs.conf
-ldconfig
--- assets/setup/by-arch/arm64v8/setup.sh
+++ assets/setup/by-arch/arm64v8/setup.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-# nothing to do
+set -e
+
+# install VC library
+apt-get update
+apt-get install -y --no-install-recommends \
+    libraspberrypi-bin
+rm -rf /var/lib/apt/lists/*
+
+set +e
