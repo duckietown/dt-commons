55dcb372ed6938ba01fd664af45ade97c94100f0
Andrea F. Daniele <afdaniele@duckietown.com>
2023-10-09 15:20:16 -0400

    minor

--- launchers/bash.sh
+++ launchers/bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# NOTE: no need to source the environment here because already done in ~/.bashrc
+# NOTE: no need to source the environment here because already done in /etc/bash.bashrc
 
 # turn to bash
 exec /bin/bash
