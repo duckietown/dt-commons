74deb19191fb341ca9437e8c68fc128c008585a3
Andrea F. Daniele <afdaniele@duckietown.com>
2024-02-16 18:06:10 -0500

    minor

--- assets/entrypoint.d/pythonpath.sh
+++ assets/entrypoint.d/pythonpath.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # make user libraries discoverable
-export PYTHONPATH=/usr/local/lib/python3/dist-packages:/usr/local/lib/python3.10/dist-packages:${PYTHONPATH}
+export PYTHONPATH=/usr/local/lib/python3/dist-packages:/usr/local/lib/python${PYTHON_VERSION}/dist-packages:${PYTHONPATH}
