68aa89a6c2210e244446cf778216c43af9b03dd9
Andrea F. Daniele <afdaniele@duckietown.com>
2024-02-29 01:48:28 -0500

    bug fix with unix socket creation for DTPS nodes

--- packages/dt_node_utils/dtps.py
+++ packages/dt_node_utils/dtps.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from typing import List, Dict
 from urllib.parse import quote
 
@@ -8,9 +9,9 @@ DTPS_NODES_PREFIX: str = "node"
 DTPS_BASE = "DTPS_BASE"
 
 
-def default_context_env(context_name: str, pure: bool = False,
+def default_context_env(context_name: str, node_name: str, pure: bool = False,
                         unix_socket_location: str = DEFAULT_DTPS_UNIX_SOCKET_LOCATION) -> dict:
-    default_urls: List[str] = default_context_urls(unix_socket_location=unix_socket_location)
+    default_urls: List[str] = default_context_urls(node_name, unix_socket_location=unix_socket_location)
     urls: Dict[str, str] = {
         f"{DTPS_BASE}_{context_name.upper()}_{i}": url for i, url in enumerate(default_urls)
     }
@@ -22,15 +23,17 @@ def default_context_env(context_name: str, pure: bool = False,
     return env
 
 
-def default_context_urls(unix_socket_location: str = DEFAULT_DTPS_UNIX_SOCKET_LOCATION) -> List[str]:
+def default_context_urls(node_name: str, unix_socket_location: str = DEFAULT_DTPS_UNIX_SOCKET_LOCATION) -> List[str]:
     urls: List[str] = [
         "create:http://0.0.0.0:0/",
     ]
     if unix_socket_location != DEFAULT_DTPS_UNIX_SOCKET_LOCATION or os.path.exists(TMPFS_DISK_LOCATION):
-        if not os.path.exists(unix_socket_location):
-            os.makedirs(unix_socket_location, exist_ok=True)
+        sock_fpath: str = os.path.join(unix_socket_location, DTPS_NODES_PREFIX, f"{node_name}.sock")
+        # make directory
+        Path(sock_fpath).parent.mkdir(parents=True, exist_ok=True)
+        # add url
         urls += [
-            f"create:http+unix://{quote(os.path.join(unix_socket_location, DTPS_NODES_PREFIX), safe='')}/"
+            f"create:http+unix://{quote(sock_fpath, safe='')}/"
         ]
     # ---
     return urls
