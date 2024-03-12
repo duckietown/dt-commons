c6dbe6989124ebc108d8de8bd784693ac77fe361
Andrea F. Daniele <afdaniele@duckietown.com>
2024-03-12 10:35:35 -0400

    avoid using mDNS with DTPS whenever possible

--- assets/entrypoint.d/10.dtps.sh
+++ assets/entrypoint.d/10.dtps.sh
@@ -4,12 +4,12 @@
 netmode=$(dt-get-network-mode)
 
 # robot's switchboard
-export DTPS_BASE_SWITCHBOARD_0="http+unix://%2Fdtps%2Fswitchboard.sock"
+export DTPS_BASE_SWITCHBOARD_0="http+unix://%2Fdtps%2Fswitchboard.sock/"
 
 
 # avoid using mDNS when running onboard the vehicle
 if [ "${DT_DEPLOYED_ONBOARD}" = "1" ] && [ "${netmode}" = "HOST" ]; then
-    export DTPS_BASE_SWITCHBOARD_1="http://localhost:11511"
+    export DTPS_BASE_SWITCHBOARD_1="http://localhost:11511/"
 else
-    export DTPS_BASE_SWITCHBOARD_1="http://${VEHICLE_NAME}.local:11511"
+    export DTPS_BASE_SWITCHBOARD_1="http://${VEHICLE_NAME}.local:11511/"
 fi
