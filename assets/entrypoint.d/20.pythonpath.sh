10f4b44e1c8358c52f3837abc704ee0750f6b040
Andrea F. Daniele <afdaniele@duckietown.com>
2024-03-17 23:03:26 -0400

    bug fix in entrypoint script 20.pythonpath.sh

--- assets/entrypoint.d/20.pythonpath.sh
+++ assets/entrypoint.d/20.pythonpath.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # make user libraries discoverable
-export PYTHONPATH=/usr/local/lib/python3/dist-packages:/usr/local/lib/python${PYTHON_VERSION}/dist-packages:${PYTHONPATH}
+export PYTHONPATH=${PYTHONPATH}:/usr/local/lib/python3/dist-packages:/usr/local/lib/python${PYTHON_VERSION}/dist-packages
