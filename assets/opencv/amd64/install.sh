91345e36208fb6934c4137bc05714b9c1da38b9f
Andrea F. Daniele <afdaniele@duckietown.com>
2024-04-21 00:54:31 -0400

    updated opencv installation scripts

--- assets/opencv/amd64/install.sh
+++ assets/opencv/amd64/install.sh
@@ -1,1 +1,1 @@
-python3 -m pip install -U opencv-python-headless==4.8.1.78 numpy==1.26.2
+python3 -m pip install -U opencv-python-headless==4.9.0.80 numpy==1.26.4
--- assets/opencv/arm64/install.sh
+++ assets/opencv/arm64/install.sh
@@ -2,5 +2,5 @@ set -e
 
 cd /tmp/
 wget --quiet https://assets.duckietown.com/python/wheels/opencv_python_headless-4.9.0.80-cp312-cp312-linux_aarch64.whl
-python3 -m pip install ./opencv_python_headless-4.9.0.80-cp312-cp312-linux_aarch64.whl
+python3 -m pip install ./opencv_python_headless-4.9.0.80-cp312-cp312-linux_aarch64.whl numpy==1.26.4
 rm ./opencv_python_headless-4.9.0.80-cp312-cp312-linux_aarch64.whl
