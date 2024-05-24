ab04716715470cdd0eedbd161878edf54df05cab
Andrea F. Daniele <afdaniele@duckietown.com>
2024-05-24 00:49:03 -0400

    bug fix

--- packages/dt_node_utils/config.py
+++ packages/dt_node_utils/config.py
@@ -213,7 +213,7 @@ class NodeConfiguration(DataClassJsonMixin, DataContainer):
                 user_fpath.as_posix(),
                 schema_path=schema_path.as_posix() if schema_path.is_file() else None
             )
-        except ValidationError as e:
+        except (ValidationError, KeyError) as e:
             print("User configuration file is invalid. "
                   "Falling back to the original configuration file.\n"
                   "The error is the following:\n\n", e, "\n")
