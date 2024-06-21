a91f9192049eacd5b9828e6cca4800e1ef6b456e
Andrea F. Daniele <afdaniele@duckietown.com>
2024-06-21 01:45:16 -0400

    minor

--- packages/dt_node_utils/config.py
+++ packages/dt_node_utils/config.py
@@ -238,7 +238,7 @@ class NodeConfiguration(DataClassJsonMixin, DataContainer):
                 user_fpath.as_posix(),
                 schema_path=schema_path.as_posix() if schema_path.is_file() else None
             )
-        except (ValidationError, KeyError) as e:
+        except (ValidationError, KeyError, AttributeError) as e:
             print("User configuration file is invalid. "
                   "Falling back to the original configuration file.\n"
                   "The error is the following:\n\n", e, "\n")
