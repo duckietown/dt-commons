8c71fef34d7bd53fa2078dbe5bbe2225308c3b86
Andrea F. Daniele <afdaniele@duckietown.com>
2024-07-06 00:39:06 -0400

    added utils dt_cli_utils.install_colored_logs

--- packages/dt_cli_utils/utils.py
+++ packages/dt_cli_utils/utils.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def str_to_bool(value) -> bool:
     value = value.lower()
     if value in ["y", "yes", "t", "true", "on", "1"]:
@@ -6,3 +9,15 @@ def str_to_bool(value) -> bool:
         return False
     else:
         raise ValueError(f"Invalid truth value {value!r}.")
+
+
+def install_colored_logs(logger: logging.Logger, level: int = None):
+    import coloredlogs
+    # NOTE: This is kinda weird, but it seems that the coloredlogs library has
+    #       some issues: https://github.com/xolox/python-coloredlogs/issues/18
+    # get the current root logger level
+    logLevel = logging.getLogger().getEffectiveLevel()
+    # install the coloredlogs on our logger and with our level
+    coloredlogs.install(level=level or logger.level, logger=logger)
+    # restore the root logger level
+    logging.getLogger().setLevel(logLevel)
